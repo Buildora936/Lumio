0e7f1b04081c8d15d75e563076f128c3dcdc6ffe
BUILDORA <bjosephbrooks07@gmail.com>
2026-01-21 16:37:21 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,5 @@
-self.addEventListener("install", () => {
-  console.log("Lumio service worker installé");
+self.addEventListener("install", e => {
+  self.skipWaiting();
 });
+
+self.addEventListener("fetch", e => {});
